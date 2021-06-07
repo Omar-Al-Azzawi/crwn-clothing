3ddb7ed558ac5c11d465577e1c9d38ac7d2f02ac
Omar Alazzawi <omaralazzawi@Omars-MBP.home>
2021-06-07 12:19:45 +0300

    Broken our homepage into smaller components

---
+++ src/pages/homepage/Homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './homepage.styles.scss';
+import DiractoryMenu from "../../components/diractory/diractory.component";
+
+function Homepage() {
+    return (
+        <div className="homepage">
+        <DiractoryMenu />
+        </div>
+    )
+}
+
+export default Homepage;
+
